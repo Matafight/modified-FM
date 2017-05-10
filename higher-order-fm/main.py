9b9fde2c89a92601db46da3aac9b472e43fd7082
Matafight <guo_sc@foxmail.com>
2017-05-10 21:24:18 +0800

    rewrite hofm using cython and add adam and rmse adadelta algorithm support

--- higher-order-fm/main.py
+++ higher-order-fm/main.py
@@ -4,6 +4,8 @@ from sklearn.feature_extraction import DictVectorizer
 from higher_fm import FM
 import mymultiprocess_crossvalidation as my_cv
 
+from hofmlib import HOFM
+
 def loadData(filename):
     data=[]
     y = []
@@ -21,8 +23,8 @@ def loadData(filename):
 if __name__=='__main__':
     train_data_name = 'u1.base'
     test_data_name = 'u1.test'
-    (train_data,train_label,train_users,train_items)= loadData('../data/'+train_data_name)
-    (test_data,test_label,test_users,test_items)=loadData('../data/'+test_data_name)
+    (train_data,train_label,train_users,train_items)= loadData('./data/'+train_data_name)
+    (test_data,test_label,test_users,test_items)=loadData('./data/'+test_data_name)
     
     train_size=  len(train_data)
     all_data = train_data + test_data
@@ -42,9 +44,12 @@ if __name__=='__main__':
     #n_test_label = all_label[2000:3000]
     
     
-    num_attributes = x_train.shape[1]
+    '''num_attributes = x_train.shape[1]
     num_factors = 10
 
     bestreg = [0.1,0.1]
     myfm = FM(verbose = True,n_iter = 200,num_factors=num_factors,num_attributes = num_attributes,dataname = train_data_name, reg_1=bestreg[0],reg_2=bestreg[0])
-    myfm.fit(x_train,train_label,x_test,test_label)
+    myfm.fit(x_train,train_label,x_test,test_label)'''
+
+    hofm_model = HOFM(reg_1 = 0.01,reg_2 = 0.01,num_iter = 100,learning_rate = 0.001)
+    hofm_model.fit(x_train,train_label,x_test,test_label)
